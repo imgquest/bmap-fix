f6e6ced719cda235259145888ccf712eceb15678
imgquest <gogogo242@gmail.com>
2019-05-08 19:09:49 +0300

    first commit

---
+++ main.lua
@@ -0,0 +1,12 @@
+﻿local BattlefieldMapFixFreeze = CreateFrame("Frame")
+BattlefieldMapFixFreeze:RegisterEvent("ZONE_CHANGED")
+BattlefieldMapFixFreeze:RegisterEvent("ZONE_CHANGED_INDOORS")
+
+BattlefieldMapFixFreeze:SetScript("OnEvent", function()
+	BattlefieldMap_LoadUI()
+
+	if ( BattlefieldMapFrame ) then
+		local mapID = MapUtil.GetDisplayableMapForPlayer()
+		BattlefieldMapFrame:SetMapID(mapID)
+	end
+end)
